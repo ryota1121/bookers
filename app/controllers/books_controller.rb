1d4ab9211e523acd5db23f263c29f2e0d5ef63fc
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2024-01-09 14:50:05 +0000

    1/9 commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
